ede6122c8f153e3ec4277a3c72aaaee2ad0596cb
todashunta <shunto325@gmail.com>
2023-02-07 14:46:01 +0900

    日付追加

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -23,7 +23,7 @@ export default function BlogId({ blog }) {
             <h1>{blog.title}</h1>
           </div>
           <div className="c-works-blog__createdAt">
-            <span className="">{day.format("YYYY/MM/DD")}</span>
+            {blog.time ? <span>{blog.time}</span> : <></>}
           </div>
           <div
             className="c-blog"
